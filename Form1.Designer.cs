30a60dc7f0424a73b95964283e62804471b382f2
Cloud Sung <clouds.sung@gmail.com>
2019-06-03 17:13:36 +0800

    Change UI element

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(1008, 323);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(256, 66);
+            this.button1.Size = new System.Drawing.Size(217, 66);
             this.button1.TabIndex = 0;
             this.button1.Text = "Transfer";
             this.button1.UseVisualStyleBackColor = true;
